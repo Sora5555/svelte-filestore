21161de05c79d58e5469875b9c3cb334fe688be2
enggar eka <enggarekamaulana@gmail.com>
2025-05-06 16:48:15 +0800

    initial commit

---
+++ src/routes/sign-up/+page.server.ts
@@ -0,0 +1,33 @@
+import type { RequestEvent, Actions } from "./$types";
+import { db } from "$lib/server/db/index";
+import { user } from "$lib/server/db/schema";
+import bcrypt from "bcrypt";
+
+export async function load(){
+    const users = await db.query.user.findMany();
+
+    return {
+        users: users
+    };
+}
+
+
+export const actions: Actions = {
+    default: async({ request }) => {
+        const data = await request.formData();
+        let username: FormDataEntryValue | null = data.get("username");
+        let password: FormDataEntryValue | null = data.get("password");
+        let reconfirm_password: FormDataEntryValue | null = data.get("reconfirm_password");
+        if(password === reconfirm_password){
+            async function hashPassword(plainText: FormDataEntryValue | null) {
+                let hashed = bcrypt.hash(plainText, 10).then(function(hash: any){
+                    return hash
+                })
+                return hashed;
+            }
+            password = await hashPassword(password)
+            const newUsers = await db.insert(user).values({username, password});
+        }
+
+    }
+}
---
+++ src/routes/sign-up/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+
+    import { enhance } from "$app/forms";
+    let { data } = $props();
+</script>
+
+
+<form method="POST" use:enhance>
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <label for="reconfirm_password">Password</label>
+    <input type="password" name="reconfirm_password" id="reconfirm_password">
+    <button type="submit">Submit</button>
+</form>
+
+<table>
+    <thead>
+        <tr>
+            <th>Username</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.users as singleData}
+            <tr>
+                <td>{singleData.username}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
