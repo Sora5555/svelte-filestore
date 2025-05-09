57f05158f36cce92513c85350f7185334f4d0c8a
enggar eka <enggarekamaulana@gmail.com>
2025-05-09 19:37:43 +0800

    added form to insert campus for admin and add the list for each kampus on admin dashboard

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -3,13 +3,15 @@ import * as auth from "$lib/server/auth"
 import { redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 import { fail } from "@sveltejs/kit";
+import { db } from "$lib/server/db";
+import { kampus } from "$lib/server/db/schema";
 
 export const load: PageServerLoad = async (event) => {
-    if(!event.locals.user){
+    if(!event.locals.user || !event.locals.role){
         redirect(302, "/login")
     }
-    console.log(event.locals.user);
-    return {user: event.locals.user, role: event.locals.role}
+    let kampus  = db.query.kampus.findMany();
+    return {user: event.locals.user, role: event.locals.role, kampus: kampus}
 }
 
 export const actions: Actions = {
@@ -21,5 +23,18 @@ export const actions: Actions = {
                 auth.deleteSessionTokenCookie(event);
         
                 return redirect(302, '/login');
+    },
+    tambahKampus: async ({request}) => {
+        let data = await request.formData();
+        let namaKampus = data.get("namaKampus");
+        let singkatanKampus = data.get("singkatanKampus");
+        if(!namaKampus || !singkatanKampus){
+            return fail(401);
+        }
+        const newKampus = await db.insert(kampus).values({ namaKampus, singkatanKampus, })
+
+    },
+    tambahSemester: async({params}) => {
+        console.log(params, 1);
     }
 }
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
     let {data} = $props();
+    let namaRole = data.role.namaRole;
 </script>
 
-<h1>Hi user {data.role?.namaRole}</h1>
+{#if namaRole == "admin"}
+<h5>Tambah kampus</h5>
+<form action="?/tambahKampus" method="post" use:enhance>
+    <label for="namaKampus">Nama Kampus</label>
+    <input type="text" name="namaKampus" id="namaKampus">
+    <label for="singkatanKampus">Singkatan Kampus</label>
+    <input type="text" name="singkatanKampus" id="singkatanKampus">
+    <button type="submit">Tambah</button>
+</form>
+{/if}
+<h1>Hi user {data.role.namaRole}</h1>
+<ul>
+    {#await data.kampus}
+        <li>Waiting for data</li>
+    {:then kampusData} 
+        {#each kampusData as kampus}
+            <li>{kampus.namaKampus} <form action="?/tambahSemester"><input type="hidden" name="idKampus" value="{kampus.id}"> <button type="submit">Tambah Semester</button></form></li>
+        {/each}
+    {/await}
+</ul>
