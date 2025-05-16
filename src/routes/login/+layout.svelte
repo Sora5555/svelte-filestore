b988701b6348025512b25d9f1a83f05610bdb1aa
enggar eka <enggarekamaulana@gmail.com>
2025-05-16 19:16:12 +0800

    Finishing the styling for the login page

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     let { data, children } = $props();
 </script>
-<div class="max-w-full h-screen flex justify-center items-center bg-[#2D2D2D] text-white">
+<img src="./Group 3.svg" alt="" class="absolute top-24 right-24"><img src="./Group 4.svg" alt="" class="absolute bottom-10 left-20"><img src="./Group 5.svg" alt="" class="absolute bottom-40 right-40"><img src="./Group 7.svg" alt="" class="absolute top-12 left-20">
+<div class="max-w-full h-screen flex justify-center items-center bg-[#2D2D2D] text-white flex-col">
     {@render children()}
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -67,7 +67,7 @@ const config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans, "'Kelly Slab"]
+				sans: [...fontFamily.sans, "'Kelly Slab", "Jetbrains Mono"]
 			},
 			keyframes: {
 				"accordion-down": {
